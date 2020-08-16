df95ade9c90558bf7cd928bed85ba373ea30fa15
David Wheatley <hi@davwheat.dev>
2020-08-16 21:24:26 +0100

    Add extra info to action strings

--- lib/models/Actions.dart
+++ lib/models/Actions.dart
@@ -21,9 +21,9 @@ enum Action {
 
 String actionToString(Action action) {
   if (action == Action.TO_TICKET_OFFICE)
-    return "Refer to ticket office";
+    return "Refer to ticket office or seek assistance";
   else if (action == Action.IRREG_TRAVEL)
-    return "Suspicious or irregular travel";
+    return "Suspicious or irregular travel - seek assistance";
   else if (action == Action.TOP_UP_CARD)
     return "Top up Oyster or Smartcard PAYG balance";
   else if (action == Action.HELPDESK)
@@ -35,21 +35,21 @@ String actionToString(Action action) {
   else if (action == Action.REFER_CARD_ISSUER)
     return "Refer to card issuer";
   else if (action == Action.TRY_AGAIN)
-    return "Try again";
+    return "Try again or seek assistance";
   else if (action == Action.TRY_AGAIN_CARD)
     return "Represent card";
   else if (action == Action.TRY_AGAIN_ONE_CARD)
     return "Present only one card";
   else if (action == Action.CHECK_RCI)
-    return "Check RCI list";
+    return "Check RCI list - seek assistance";
   else if (action == Action.CONTACT_CTS)
     return "Contact the Cubic Transportation Systems Report Centre";
   else if (action == Action.DISABLE_TEST_MODE)
-    return "Disable test mode";
+    return "Disable test mode - seek assistance";
   else if (action == Action.REPLACE_TICKET)
-    return "Replace ticket";
+    return "Replace ticket - seek assistance";
   else if (action == Action.REPLACE_ITSO_CARD)
-    return "Replace Smartcard";
+    return "You need a replacement Smartcard - seek assistance";
   else if (action == Action.REPRESENT_ONE_CARD)
     return "Represent one card only";
   else if (action == Action.SEEK_ASSISTANCE)
