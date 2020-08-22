1ad8f487277e991f8527812920865b44bf433c53
David Wheatley <hi@davwheat.dev>
2020-08-22 15:02:32 +0100

    Add banner ad

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
+import 'package:gateline_errors/helpers/ad_manager.dart';
 import 'screens/enterCode.dart';
 
 void main() {
@@ -9,13 +11,47 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    _initAdMob();
+
     return MaterialApp(
       title: 'Gateline Errors',
       theme: ThemeData(
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: EnterGatelineCodePage(),
+      home: FutureBuilder<void>(
+        future: _initAdMob(),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<void> snapshot,
+        ) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Gateline Errors"),
+              ),
+              body: EnterGatelineCodePage(),
+            );
+          } else {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Gateline Errors"),
+              ),
+              body: Center(
+                child: SizedBox(
+                  child: CircularProgressIndicator(),
+                  width: 48,
+                  height: 48,
+                ),
+              ),
+            );
+          }
+        },
+      ),
     );
+  }
+
+  Future<void> _initAdMob() {
+    return FirebaseAdMob.instance.initialize(appId: AdManager.appId);
   }
 }
--- lib/screens/enterCode.dart
+++ lib/screens/enterCode.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../helpers/ad_manager.dart';
 import '../widgets/codeInfo.dart';
 import '../widgets/glossary.dart';
 
@@ -14,11 +16,33 @@ class EnterGatelineCodePage extends StatefulWidget {
 class _EnterGatelineCodePageState extends State<EnterGatelineCodePage> {
   final _formKey = GlobalKey<FormState>();
 
+  BannerAd _bannerAd;
+
   int _code;
 
   @override
   void initState() {
     super.initState();
+
+    _bannerAd = BannerAd(
+      adUnitId: AdManager.bannerAdUnitId,
+      size: AdSize.banner,
+    );
+
+    _loadBannerAd();
+  }
+
+  void _loadBannerAd() {
+    _bannerAd
+      ..load()
+      ..show(anchorType: AnchorType.bottom);
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -26,85 +50,74 @@ class _EnterGatelineCodePageState extends State<EnterGatelineCodePage> {
     int c = _code ?? -1;
     GatelineCodeInfo codeInfo = GatelineCodeInfo(c);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Gateline Errors"),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: SingleChildScrollView(
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          physics: BouncingScrollPhysics(),
-          child: Padding(
-            padding: EdgeInsets.all(12),
-            child: Column(
-              children: <Widget>[
-                Card(
-                  elevation: 2,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(24, 16, 24, 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Form(
-                          key: _formKey,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              TextFormField(
-                                maxLength: 3,
-                                onChanged: (value) {
-                                  try {
-                                    int code = int.parse(value);
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: SingleChildScrollView(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            children: <Widget>[
+              Card(
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(24, 16, 24, 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            TextFormField(
+                              maxLength: 3,
+                              onChanged: (value) {
+                                try {
+                                  int code = int.parse(value);
 
+                                  setState(() {
+                                    this._code = code;
+                                  });
+                                } catch (e) {
+                                  if (value == "") {
+                                    // set code to -1 when field is empty
                                     setState(() {
-                                      this._code = code;
+                                      this._code = -1;
                                     });
-                                  } catch (e) {
-                                    if (value == "") {
-                                      // set code to -1 when field is empty
-                                      setState(() {
-                                        this._code = -1;
-                                      });
-                                    }
                                   }
-                                },
-                                decoration:
-                                    InputDecoration(hintText: "Gateline code"),
-                                autocorrect: false,
-                                keyboardType: TextInputType.numberWithOptions(
-                                  signed: false,
-                                  decimal: false,
-                                ),
-                                inputFormatters: [
-                                  WhitelistingTextInputFormatter.digitsOnly
-                                ],
-                                autofocus: true,
-                              )
-                            ],
-                          ),
+                                }
+                              },
+                              decoration:
+                                  InputDecoration(hintText: "Gateline code"),
+                              autocorrect: false,
+                              keyboardType: TextInputType.numberWithOptions(
+                                signed: false,
+                                decimal: false,
+                              ),
+                              inputFormatters: [
+                                WhitelistingTextInputFormatter.digitsOnly
+                              ],
+                              autofocus: true,
+                            )
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-                codeInfo,
-                c >= 100 ? NRKeyGlossary() : SizedBox.shrink(),
-              ],
-            ),
+              ),
+              codeInfo,
+              c >= 100 ? NRKeyGlossary() : SizedBox.shrink(),
+            ],
           ),
         ),
       ),
-
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
