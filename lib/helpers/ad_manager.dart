21d93b33f242405d5ab2eaf845635328f9c869df
David Wheatley <hi@davwheat.dev>
2020-08-22 15:45:02 +0100

    Update ad IDs

--- lib/helpers/ad_manager.dart
+++ lib/helpers/ad_manager.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
+
 class AdManager {
   static String get appId {
     if (Platform.isAndroid) {
@@ -13,34 +15,35 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      // return "ca-app-pub-2701335557132384/6293742813";
-      return "ca-app-pub-3940256099942544/6300978111";
-    } /* else if (Platform.isIOS) {
-      return "<YOUR_IOS_BANNER_AD_UNIT_ID>";
-    } */
-    else {
-      // throw new UnsupportedError("Unsupported platform");
+      if (kDebugMode) {
+        // TEST AD ID
+        return "ca-app-pub-3940256099942544/6300978111";
+      }
+
+      // REAL AD ID
+      return "ca-app-pub-2701335557132384/6293742813";
+    } else {
       return "";
     }
   }
 
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return "<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>";
-    } else if (Platform.isIOS) {
-      return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
+  // static String get interstitialAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>";
+  //   } else if (Platform.isIOS) {
+  //     return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
 
-  static String get rewardedAdUnitId {
-    if (Platform.isAndroid) {
-      return "<YOUR_ANDROID_REWARDED_AD_UNIT_ID>";
-    } else if (Platform.isIOS) {
-      return "<YOUR_IOS_REWARDED_AD_UNIT_ID>";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
+  // static String get rewardedAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "<YOUR_ANDROID_REWARDED_AD_UNIT_ID>";
+  //   } else if (Platform.isIOS) {
+  //     return "<YOUR_IOS_REWARDED_AD_UNIT_ID>";
+  //   } else {
+  //     throw new UnsupportedError("Unsupported platform");
+  //   }
+  // }
 }
