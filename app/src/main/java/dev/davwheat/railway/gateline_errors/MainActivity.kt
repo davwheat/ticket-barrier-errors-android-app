053da6d944dbc4ee31a8899562010a8ac2e41a4a
David Wheatley <david@davwheat.dev>
2025-02-01 21:40:59 +0000

    feat: edge to edge

--- app/src/main/java/dev/davwheat/railway/gateline_errors/MainActivity.kt
+++ app/src/main/java/dev/davwheat/railway/gateline_errors/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.davwheat.railway.gateline_errors
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,6 +28,8 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
 
     setContent {
+      enableEdgeToEdge()
+
       var showAds by rememberSaveable { mutableStateOf(false) }
 
       LaunchedEffect(true) {
