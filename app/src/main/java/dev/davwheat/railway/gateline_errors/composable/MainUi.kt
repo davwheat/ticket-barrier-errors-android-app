87022dfd003e28c20347d41536aaf3b40c2dd0e6
David Wheatley <david@davwheat.dev>
2025-02-11 21:53:53 +0000

    fix: ime padding

--- app/src/main/java/dev/davwheat/railway/gateline_errors/composable/MainUi.kt
+++ app/src/main/java/dev/davwheat/railway/gateline_errors/composable/MainUi.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -56,7 +57,7 @@ fun MainUi(modifier: Modifier = Modifier, showAds: Boolean) {
                 )
         )
 
-    Scaffold(modifier) { innerPadding ->
+    Scaffold(modifier.imePadding()) { innerPadding ->
         Box(modifier = Modifier.fillMaxSize()) {
             BottomSheetScaffold(
                 sheetDragHandle = {},
