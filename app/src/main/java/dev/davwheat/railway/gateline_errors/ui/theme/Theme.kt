520721ea5e720bb02495b578f1ab335a0d1d4bd8
David Wheatley <david@davwheat.dev>
2025-02-11 21:54:11 +0000

    feat: dynamic themes

--- app/src/main/java/dev/davwheat/railway/gateline_errors/ui/theme/Theme.kt
+++ app/src/main/java/dev/davwheat/railway/gateline_errors/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package dev.davwheat.railway.gateline_errors.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val LightColors =
     lightColorScheme(
@@ -74,12 +78,19 @@ private val DarkColors =
 
 @Composable
 fun AppTheme(useDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors =
-        if (!useDarkTheme) {
-            LightColors
-        } else {
-            DarkColors
+    val supportsDynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+    val colorScheme =
+        when {
+            supportsDynamicColor && useDarkTheme -> {
+                dynamicDarkColorScheme(LocalContext.current)
+            }
+            supportsDynamicColor && !useDarkTheme -> {
+                dynamicLightColorScheme(LocalContext.current)
+            }
+            useDarkTheme -> DarkColors
+            else -> LightColors
         }
 
-    MaterialTheme(colorScheme = colors, content = content)
+    MaterialTheme(colorScheme = colorScheme, content = content)
 }
